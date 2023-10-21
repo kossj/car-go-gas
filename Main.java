318a723d2f9fa614b0678f5d6c17bdc1b8c2d1bf
Jack K <52486731+kossj@users.noreply.github.com>
2023-10-21 04:41:54 +0000

    Final

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Car a = new Car(20, 20, 28, 400);
+        Car a = new Car(12.5, 12.5, 24, 200);
         Random r = new Random();
 
         int stops = 0;
@@ -28,6 +28,7 @@ public class Main {
         System.out.println("---------------------------------");
         System.out.println();
         System.out.println(a.toString());
+        System.out.format("Stops: %d\n", stops);
         System.out.format("Total Distance: %.1f mi\n", a.getTotalMiles());
         System.out.format("Average Distance: %.1f mi\n", sumDistance/stops);
         System.out.format("Average PPG: $%.2f\n", sumPricePerGal/stops);
