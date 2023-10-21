f99ef2fadcc7ee8a25d750c1b59ec01c9f1eb8f0
Jack K <52486731+kossj@users.noreply.github.com>
2023-10-21 04:08:46 +0000

    random working

--- Main.java
+++ Main.java
@@ -5,18 +5,20 @@ public class Main {
         Car a = new Car(12.5, 12.5, 24);
         Random r = new Random();
 
+        
+
         int stops = 0;
         double sumPricePerGal = 0;
         double sumDistance = 0;
 
         while (a.getMoney() >= 50) {
-            double distance = r.nextDouble(50, a.getMilesToEmpty());
-            double price_per_gal = r.nextDouble(2.49, 4.29);
+            double distance = randomInRange(r, 50, a.getMilesToEmpty());
+            double price_per_gal = randomInRange(r, 2.49, 4.29);
 
             a.drive(distance);
             System.out.println();
 
-            double gallons_to_buy = r.nextDouble(50 / a.getMPG(), a.getTankCapacity() - a.getCurrentGas());
+            double gallons_to_buy = randomInRange(r, 50 / a.getMPG(), a.getTankCapacity() - a.getCurrentGas());
             a.fillGas(gallons_to_buy, price_per_gal);
             System.out.println();
 
@@ -31,5 +33,9 @@ public class Main {
         System.out.format("Average Distance: %.1f mi\n", sumDistance/stops);
         System.out.format("Average PPG: $%.1f\n", sumPricePerGal/stops);
 
+    }
+
+    private static double randomInRange(Random r, double min, double max) {
+        return min + (max - min) * r.nextDouble();
     }
 }
